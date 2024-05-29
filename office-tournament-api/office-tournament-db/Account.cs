cfe1aa76aa7335a0904dbc115f2402dcce59c3c8
Fredrik Dyngeland <fredrik_dy92@hotmail.com>
2024-05-29 13:55:35 +0200

    added db classes

---
+++ office-tournament-api/office-tournament-db/Account.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace office_tournament_api.office_tournament_db
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        [ForeignKey(nameof(TournamentId))]
+        public Tournament Tournament { get; set; }
+        public Guid TournamentId {  get; set; }
+        [MaxLength(100)]
+        public string Email { get; set; }
+        [MaxLength(50)]
+        public string UserName { get; set; }
+        public int Score { get; set; }
+        public int MatchesWon { get; set; }
+        public int MatchesPlayed { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ office-tournament-api/office-tournament-db/Match.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace office_tournament_api.office_tournament_db
+{
+    public class Match
+    {
+        public Guid Id { get; set; }
+        [ForeignKey(nameof(TournamentId))]
+        public Tournament Tournament { get; set; }
+        public Guid TournamentId { get; set; }
+
+        [ForeignKey(nameof(WinnerId))]
+        public Account Winner {  get; set; }
+        public Guid WinnerId { get; set; }
+
+        [ForeignKey(nameof(LoserId))]
+        public Account Loser { get; set; }
+        public Guid LoserId { get; set; }
+        public int LoserDeltaScore { get; set; }
+        public DateTime Date {  get; set; }
+    }
+}

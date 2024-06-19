726a94c911770bbd0392708413e83bfa84de6bac
Fredrik Dyngeland <fredrik_dy92@hotmail.com>
2024-06-19 15:57:30 +0200

    added isActive field to Tournament

---
+++ office-tournament-api/Migrations/20240619135643_AddedIsActiveToTournament.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace office_tournament_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsActiveToTournament : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Tournaments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Tournaments");
+        }
+    }
+}
